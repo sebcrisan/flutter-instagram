de679e230e9a402d359ae8db3d9948ba9da0b3d4
sebcrisan <sebastiaancrisan@gmail.com>
2023-05-23 16:28:06 +0200

    feat: add ensureinitialized

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'firebase_options.dart';
 
 // provider scope
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
