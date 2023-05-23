9dc29077d3ab5c4e1e755bea684b24bb6dfefa20
sebcrisan <sebastiaancrisan@gmail.com>
2023-05-23 16:05:46 +0200

    feat: rm boilerplate

--- lib/main.dart
+++ lib/main.dart
@@ -8,193 +8,17 @@ void main() {
   ));
 }
 
-// Film class
-@immutable
-class Film {
-  final String id;
-  final String title;
-  final String description;
-  final bool isFavorite;
-
-  const Film({
-    required this.id,
-    required this.title,
-    required this.description,
-    required this.isFavorite,
-  });
-
-  Film copy({
-    required bool isFavorite,
-  }) =>
-      Film(
-        id: id,
-        title: title,
-        description: description,
-        isFavorite: isFavorite,
-      );
-
-  // toString
-  @override
-  String toString() => 'Film(id: $id, '
-      'title: $title, '
-      'description: $description, '
-      'isFavorite: $isFavorite)';
-
-  // equality
-  @override
-  bool operator ==(covariant Film other) =>
-      id == other.id && isFavorite == other.isFavorite;
-
-  @override
-  int get hashCode => Object.hashAll([id, isFavorite]);
-}
-
-// List of films hardcoded
-const allFilms = [
-  Film(
-    id: '1',
-    title: 'The Shawshank Redemption',
-    description: 'Description for The Shawshank Redemption',
-    isFavorite: false,
-  ),
-  Film(
-    id: '2',
-    title: 'The Godfather',
-    description: 'Description for The Godfather',
-    isFavorite: false,
-  ),
-  Film(
-    id: '3',
-    title: 'The Godfather Part II',
-    description: 'Description for The Godfather Part II',
-    isFavorite: false,
-  ),
-  Film(
-    id: '4',
-    title: 'The Dark Knight',
-    description: 'Description for The Dark Knight',
-    isFavorite: false,
-  ),
-];
-
-// Films notifier that has a list of films and allows to update any given film with a favorite flag
-class FilmsNotifier extends StateNotifier<List<Film>> {
-  /// By default is is populated with a list of all films
-  FilmsNotifier() : super(allFilms);
-
-  /// Update film state
-  void update(Film film, bool isFavorite) {
-    /// Loop through all films, if id matches we can update it
-    /// map => toList() is rly nice
-    state = state
-        .map((thisFilm) => thisFilm.id == film.id
-        ? thisFilm.copy(isFavorite: isFavorite)
-        : thisFilm)
-        .toList();
-  }
-}
-
-// Different status possibilities
-enum FavoriteStatus { all, favorite, notFavorite }
-
-// Favorite status provider that refers to all different status possibilities
-final favoriteStatusProvider = StateProvider<FavoriteStatus>(
-      (ref) => FavoriteStatus.all,
-);
-
-// All films provider
-final allFilmsProvider = StateNotifierProvider<FilmsNotifier, List<Film>>(
-      (ref) => FilmsNotifier(),
-);
-
-// Favorite films provider, utilizes the providers already available to us
-// so it can afford to be a regular provider
-final favoriteFilmsProvider = Provider<Iterable<Film>>(
-      (ref) => ref.watch(allFilmsProvider).where(
-        (film) => film.isFavorite,
-  ),
-);
-
-// Not favorite films
-final notFavoriteFilmsProvider = Provider<Iterable<Film>>(
-      (ref) => ref.watch(allFilmsProvider).where(
-        (film) => !film.isFavorite,
-  ),
-);
-
-// Films list widget
-class FilmsList extends ConsumerWidget {
-  // AlwaysAliveProvider is a mixin, shared by Provider and StateNotifierProvider
-  final AlwaysAliveProviderBase<Iterable<Film>> provider;
-
-  const FilmsList({required this.provider, Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final films = ref.watch(provider);
-    return Expanded(
-        child: ListView.builder(
-          itemCount: films.length,
-          itemBuilder: (context, index) {
-            final film = films.elementAt(index);
-            final favoriteIcon = film.isFavorite
-                ? const Icon(Icons.favorite)
-                : const Icon(Icons.favorite_border);
-            return ListTile(
-              title: Text(film.title),
-              subtitle: Text(film.description),
-              trailing: IconButton(
-                icon: favoriteIcon,
-                onPressed: () {
-                  final isFavorite = !film.isFavorite;
-                  ref.read(allFilmsProvider.notifier).update(film, isFavorite);
-                },
-              ),
-            );
-          },
-        ));
-  }
-}
-
-// Filter widget
-class FilterWidget extends StatelessWidget {
-  const FilterWidget({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Consumer(
-      builder: (context, ref, child) {
-        // enum split returns favoritestatus.all, movies.all, etc so we split by the dot
-        return DropdownButton(
-          value: ref.watch(favoriteStatusProvider),
-          items: FavoriteStatus.values
-              .map(
-                (fs) => DropdownMenuItem(
-              value: fs,
-              child: Text(fs.toString().split('.').last),
-            ),
-          )
-              .toList(),
-          onChanged: (fs) {
-            ref.read(favoriteStatusProvider.notifier).state = fs!;
-          },
-        );
-      },
-    );
-  }
-}
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Movies App',
+      title: 'Instaclone',
       darkTheme: ThemeData.dark(),
       themeMode: ThemeMode.dark,
       debugShowCheckedModeBanner: false,
-      home: const MyHomePage(title: 'Movies App'),
+      home: const MyHomePage(title: 'Instaclone'),
     );
   }
 }
@@ -211,23 +35,7 @@ class MyHomePage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Movies'),
-      ),
-      body: Column(
-        children: [
-          const FilterWidget(),
-          Consumer(builder: (context, ref, child) {
-            final filter = ref.watch(favoriteStatusProvider);
-            switch (filter) {
-              case FavoriteStatus.all:
-                return FilmsList(provider: allFilmsProvider);
-              case FavoriteStatus.favorite:
-                return FilmsList(provider: favoriteFilmsProvider);
-              case FavoriteStatus.notFavorite:
-                return FilmsList(provider: notFavoriteFilmsProvider);
-            }
-          })
-        ],
+        title: const Text('Instaclone'),
       ),
     );
   }
